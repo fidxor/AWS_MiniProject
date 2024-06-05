0ce3619748b7c94c92ed7e6c738db62a05d6e240
0415bichan <0415bichan@gmail.com>
2024-06-05 13:53:10 +0900

    0605 EC2_Stop.py
    0605 EC2_Stop.py

--- src/EC2_Stop.py
+++ src/EC2_Stop.py
@@ -1,20 +1,7 @@
-import boto3
-
-# AWS 자격 증명 설정 (환경 변수나 aws configure를 사용하여 설정된 상태여야 합니다)
-aws_access_key_id = 'AKIAYS2NWAAC7EGXENS4'
-aws_secret_access_key = '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'
-region_name = 'ap-northeast-2'
-
-# 클라이언트 생성
-ec2 = boto3.client('ec2', 
-                   aws_access_key_id=aws_access_key_id, 
-                   aws_secret_access_key=aws_secret_access_key, 
-                   region_name=region_name)
-
 # 특정 태그를 가진 EC2 인스턴스 검색
-def get_instance_id_by_tag(tag_filters):
+def get_instance_id_by_tag(ec2, filters):
     response = ec2.describe_instances(
-        Filters=tag_filters
+        Filters=filters
     )
     instances = response['Reservations']
     if not instances:
@@ -26,30 +13,19 @@ def get_instance_id_by_tag(tag_filters):
     return instance_ids
 
 # EC2 인스턴스 중지
-def stop_ec2_instance(instance_ids):
+def stop_ec2_instance(ec2, instance_ids):
     response = ec2.stop_instances(InstanceIds=instance_ids)
     print(f'Stopping EC2 instances {instance_ids}')
     return response
 
 # 메인 함수
-def main():
-    # 중지할 인스턴스의 태그
-    tag_key1 = 'Name'
-    tag_value1 = 'db_1141'#input(f"Enter the value for the tag '{tag_key1}': ")
-
-    tag_key2 = 'Creator'
-    tag_value2 = 'jessy'#input(f"Enter the value for the tag '{tag_key2}': ")
-
+def stopEC2(ec2, tagName, tagValue):
     # 태그 필터 구성
-    tag_filters = [
-        {'Name': f'tag:{tag_key1}', 'Values': [tag_value1]},
-        {'Name': f'tag:{tag_key2}', 'Values': [tag_value2]}
+    filters = [
+        {'Name': tagName,'Values': tagValue}
     ]
     
-    instance_ids = get_instance_id_by_tag(tag_filters)
+    instance_ids = get_instance_id_by_tag(filters)
     
     if instance_ids:
-        stop_ec2_instance(instance_ids)
-
-if __name__ == '__main__':
-    main()
+        stop_ec2_instance(ec2, instance_ids)
